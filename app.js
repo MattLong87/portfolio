c50a2ddf252588f8096d27381b6f88775d178130
Matthew Long <MattLong87@gmail.com>
2017-06-22 14:42:00 -0400

    tooltips for icons

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 $('#arden').hover(
-    function(){
+    function () {
         $('.matt').attr('src', './img/arden.png');
     },
-function(){
+    function () {
         $('.matt').attr('src', './img/matt.png');
     }
 )
+
+$(".icon-container").hover(function (e) {
+    $(this).children(".tooltip").toggleClass("hidden");
+})
